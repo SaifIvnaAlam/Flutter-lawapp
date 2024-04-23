c9d87a5d8d170f563d6dde23b73600b913339f98
saif ivna alam <alam.saifivna@gmail.com>
2024-04-23 06:01:00 +0600

    add: navigation bar

---
+++ lib/utils/widgets/nav_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:lawapp/utils/constants/app_colors.dart';
+import 'package:lawapp/features/home/presentation/home_page.dart';
+import 'package:lawapp/features/profile/presentaion/user_profile.dart';
+// ignore_for_file: library_private_types_in_public_api
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomePage(),
+    const ProfilePage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.accentColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
