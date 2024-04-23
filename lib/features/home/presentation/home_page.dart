f186a1fe917e6aa97bc37c5b062fad46fd03aabb
saif ivna alam <alam.saifivna@gmail.com>
2024-04-23 06:01:27 +0600

    add: ui elements

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:lawapp/utils/constants/app_radius.dart';
+import 'package:lawapp/utils/constants/app_spacing.dart';
+import 'package:lawapp/utils/constants/app_text_Style.dart';
+import 'package:lawapp/utils/widgets/primary_scaffold.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
-      ),
-    );
+    return PrimaryScaffold(
+        body: Column(
+      children: [
+        const Text(
+          "Welcome",
+          style: headingTextStyle,
+        ),
+        heightBox40,
+        Container(
+          height: 500,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey,
+            ),
+            borderRadius: primaryContainerRadius,
+          ),
+          child: Container(),
+        ),
+      ],
+    ));
   }
 }
