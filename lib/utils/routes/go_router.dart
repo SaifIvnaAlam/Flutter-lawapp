d49899e6368eeb39cc548bc6a09c0cdb98b0823b
saif ivna alam <alam.saifivna@gmail.com>
2024-04-23 06:02:16 +0600

    add: routing

--- lib/utils/routes/go_router.dart
+++ lib/utils/routes/go_router.dart
@@ -1,7 +1,9 @@
 import 'package:go_router/go_router.dart';
-import 'package:lawapp/auth/presentation/authentication_page.dart';
-import 'package:lawapp/features/home/presentation/home_page.dart';
+import 'package:lawapp/utils/widgets/nav_bar.dart';
 import 'package:lawapp/utils/routes/route_name.dart';
+import 'package:lawapp/features/home/presentation/home_page.dart';
+import 'package:lawapp/auth/presentation/authentication_page.dart';
+import 'package:lawapp/features/profile/presentaion/user_profile.dart';
 
 // GoRouter configuration
 final goRouter = GoRouter(
@@ -9,12 +11,20 @@ final goRouter = GoRouter(
   debugLogDiagnostics: true,
   routes: [
     GoRoute(
-      path: AppRoutes.HOMEPAGE,
+      path: AppRoutes.HOME_PAGE,
       builder: (context, state) => const HomePage(),
     ),
     GoRoute(
       path: AppRoutes.INITIAL_ROUTE,
       builder: (context, state) => const AuthenticationPage(),
+    ),
+    GoRoute(
+      path: AppRoutes.NAVBAR,
+      builder: (context, state) => NavBar(),
+    ),
+    GoRoute(
+      path: AppRoutes.PROFILE_PAGE,
+      builder: (context, state) => const ProfilePage(),
     ),
   ],
 );
--- lib/utils/routes/route_name.dart
+++ lib/utils/routes/route_name.dart
@@ -2,5 +2,7 @@
 
 class AppRoutes {
   static const String INITIAL_ROUTE = "/";
-  static const String HOMEPAGE = "/home";
+  static const String HOME_PAGE = "/home";
+  static const String NAVBAR = "/nav_bar";
+  static const String PROFILE_PAGE = "/nav_bar";
 }
