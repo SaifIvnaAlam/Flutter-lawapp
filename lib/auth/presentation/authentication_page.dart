0be0ff56194c246aeb7fbbb494502cdc0c4f8688
saif ivna alam <alam.saifivna@gmail.com>
2024-04-23 06:00:37 +0600

    update: cleaned ui

--- lib/auth/presentation/authentication_page.dart
+++ lib/auth/presentation/authentication_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lawapp/utils/routes/route_name.dart';
+import 'package:lawapp/utils/constants/app_assets.dart';
 import 'package:lawapp/utils/constants/app_spacing.dart';
+import 'package:lawapp/utils/constants/app_strings.dart';
 import 'package:lawapp/utils/constants/app_text_Style.dart';
+import 'package:lawapp/utils/routes/route_name.dart';
 import 'package:lawapp/utils/widgets/primary_scaffold.dart';
 import 'package:lawapp/utils/widgets/rounded_material_button.dart';
+import 'package:lottie/lottie.dart';
 
 class AuthenticationPage extends StatelessWidget {
   const AuthenticationPage({super.key});
@@ -13,19 +16,31 @@ class AuthenticationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return PrimaryScaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("WELCOME", style: headingTextStyle),
-          heightBox124,
+          heightBox40,
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(AppStrings.welcomeHeading, style: headingTextStyle),
+              Text(
+                AppStrings.welcomePeragraph,
+                style: peragraphStyle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          const Spacer(),
+          Lottie.asset(AppAssets.catAnimation),
           RoundedMaterialButton(
             ontap: () {
-              context.pushReplacement(AppRoutes.HOMEPAGE);
+              context.pushReplacement(AppRoutes.NAVBAR);
             },
-            text: 'Sign in with Google',
+            text: 'Continue with Google',
             showIcon: false,
           ),
           const SizedBox(height: 20),
           const Text("Continue without signing in"),
+          heightBox16,
         ],
       ),
     );
